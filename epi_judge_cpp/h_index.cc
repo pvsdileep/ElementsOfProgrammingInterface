034fb4eaa0c64ac7fa6f2f4f605d38c0c53870ff
Dileep Pasumarthi <pvsdileep@gmail.com>
2019-03-21 08:50:26 -0700

    hindex o(n) soln

--- epi_judge_cpp/h_index.cc
+++ epi_judge_cpp/h_index.cc
@@ -1,7 +1,12 @@
 #include <vector>
+#include <algorithm>
+
 #include "test_framework/generic_test.h"
 using std::vector;
-int HIndex(vector<int> citations) {
+using namespace std;
+
+//below is O(logN) soln
+int HIndex_logn(vector<int> citations) {
   sort(citations.begin(),citations.end());
 
   for(int i=0;i<citations.size();i++)
@@ -9,6 +14,29 @@ int HIndex(vector<int> citations) {
     if(citations[i]>=citations.size()-i)
     {
       return citations.size()-i;
+    }
+  }
+
+  return 0;
+}
+
+//below is O(n) soln
+int HIndex(vector<int> citations) {
+  int n = citations.size();
+  vector<int> s(n+1,0);
+
+  for(int i=0;i<n;i++)
+  {
+    s[min(n,citations[i])]++;
+  }
+
+  int sum =0;
+  for(int i=n;i>=0;i--)
+  {
+    sum += s[i];
+    if(sum>=i)
+    {
+      return i;
     }
   }
 
