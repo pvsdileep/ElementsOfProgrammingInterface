1211b98afabcb04c726f096b8a21f3cdf5a8f24e
Dileep Pasumarthi <pvsdileep@gmail.com>
2019-03-21 08:21:54 -0700

    h_index

--- epi_judge_cpp/h_index.cc
+++ epi_judge_cpp/h_index.cc
@@ -2,7 +2,16 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 int HIndex(vector<int> citations) {
-  // TODO - you fill in here.
+  sort(citations.begin(),citations.end());
+
+  for(int i=0;i<citations.size();i++)
+  {
+    if(citations[i]>=citations.size()-i)
+    {
+      return citations.size()-i;
+    }
+  }
+
   return 0;
 }
 
